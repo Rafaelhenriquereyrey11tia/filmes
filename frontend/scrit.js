54e475cdcdf6ceff79923a27701b4835a084988c
Rafaelhenriquereyrey11tia <162190695+Rafaelhenriquereyrey11tia@users.noreply.github.com>
2026-08-20 11:44:07 -0300

    mudança do fetch

--- frontend/scrit.js
+++ frontend/scrit.js
@@ -1,6 +1,6 @@
 async function buscarFilmes() {
     // ir ao backend, acessar a rota GET e mostrar os filmes na tela.
-    const resposta = await fetch("http://localhost:8080/all-movies")
+    const resposta = await fetch("https://filmes-front-silk.vercel.app/all-movies")
     const filmes = await resposta.json()
     const sectionFilmes = document.querySelector(".filmes")
 
